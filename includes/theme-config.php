177a3ac7220766e68df99672542febfbc97c1001
Ryan <rlsystems@outlook.com>
2025-02-20 15:59:40 -0500

    namecheck3

--- includes/theme-config.php
+++ includes/theme-config.php
@@ -200,31 +200,48 @@ add_filter( 'wpforms_process_honeypot', 'forms_custom_honeypot', 10, 4 );
 
 // Same Name Spam Check
 function custom_name_validation( $errors, $form_data ) {
-   // Get form fields
-   $fields = $form_data['fields'];
-   $name_field_id = null;
-   
-   // Loop through fields to find the one with your custom CSS class
-   foreach ($fields as $id => $field) {
-       // Check if the field has CSS classes defined
-       if (isset($field['css']) && strpos($field['css'], 'name-check-field') !== false) {
-           $name_field_id = $id;
-           break;
-       }
-   }
-   
-   // If we found a field with the custom class
-   if ($name_field_id) {
-       $first_name = isset($_POST['wpforms']['fields'][$name_field_id]['first']) ? 
-           sanitize_text_field($_POST['wpforms']['fields'][$name_field_id]['first']) : '';
-       $last_name = isset($_POST['wpforms']['fields'][$name_field_id]['last']) ? 
-           sanitize_text_field($_POST['wpforms']['fields'][$name_field_id]['last']) : '';
-       
-       if (strtolower($first_name) === strtolower($last_name) && !empty($first_name)) {
-           $errors[$form_data['id']]['header'] = esc_html__('First name and last name cannot be identical.', 'wpforms');
-       }
-   }
-   
-   return $errors;
+    // Get form fields
+    $fields = $form_data['fields'];
+    $name_field_id = null;
+    $is_multipage = false;
+    $current_page = !empty($_POST['wpforms']['page']) ? absint($_POST['wpforms']['page']) : 1;
+    
+    // Check if this is the newsletter form
+    if ($form_data['id'] == get_field('newsletter_form_id', 'options')) {
+        $is_multipage = true;
+    }
+    
+    // Loop through fields to find the one with your custom CSS class
+    foreach ($fields as $id => $field) {
+        // Check if the field has CSS classes defined
+        if (isset($field['css']) && strpos($field['css'], 'name-check-field') !== false) {
+            $name_field_id = $id;
+            break;
+        }
+    }
+    
+    // If we found a field with the custom class
+    if ($name_field_id) {
+        // Only proceed if the field data was submitted
+        // This is important for multi-page forms where not all fields are available at once
+        if (isset($_POST['wpforms']['fields'][$name_field_id])) {
+            $first_name = isset($_POST['wpforms']['fields'][$name_field_id]['first']) ? 
+                sanitize_text_field($_POST['wpforms']['fields'][$name_field_id]['first']) : '';
+            $last_name = isset($_POST['wpforms']['fields'][$name_field_id]['last']) ? 
+                sanitize_text_field($_POST['wpforms']['fields'][$name_field_id]['last']) : '';
+            
+            if (strtolower($first_name) === strtolower($last_name) && !empty($first_name)) {
+                // For multi-page forms, use field-specific error instead of header error
+                if ($is_multipage) {
+                    $errors[$form_data['id']][$name_field_id] = esc_html__('First name and last name cannot be identical.', 'wpforms');
+                } else {
+                    // For regular forms, use the header error as before
+                    $errors[$form_data['id']]['header'] = esc_html__('First name and last name cannot be identical.', 'wpforms');
+                }
+            }
+        }
+    }
+    
+    return $errors;
 }
 add_filter( 'wpforms_process_initial_errors', 'custom_name_validation', 10, 2 );
