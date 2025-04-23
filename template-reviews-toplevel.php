f6ab5af1b43987c2d81074632bc30e78e2fe5d7f
Ryan <rlsystems@outlook.com>
2025-04-23 11:38:45 -0500

    reviews

--- template-reviews-toplevel.php
+++ template-reviews-toplevel.php
@@ -28,7 +28,7 @@ get_header();
 
 
         <div class="reviews-content">
-                <div data-token="sB4qs94MlVnG05rLfF2EUUiT7tsB6XPh68n78c25jQls4RGP34" class="romw-reviews"></div>
+        <div data-romw-token="EFgjaIlLK5iHsds644tfZmrAoCT43DVczPWxRc0emY6YfzXkr0"></div>
         </div>
 
 
@@ -37,7 +37,6 @@ get_header();
 
 
 </main>
-<script src="https://reviewsonmywebsite.com/js/embedLoader.js?id=03d30ab029f3709eb7ee" type="text/javascript"></script>
-
+<script src=" https://reviewsonmywebsite.com/js/v2/embed.js?id=8ed8fd45a2fd062872f56952886c1ec5 " type="text/javascript"></script>
 
 <?php get_footer(); ?>
