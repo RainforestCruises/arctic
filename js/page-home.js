9c77d706487e58f555cfe1395e1b92541966fcc1
Ryan <rlsystems@outlook.com>
2025-05-16 12:09:00 -0500

    video home fix

--- js/page-home.js
+++ js/page-home.js
@@ -1,192 +1,177 @@
 jQuery(document).ready(function ($) {
+  // On Click - Nav Links, href change position
+  $(".hero-item").click(function (event) {
+    var id = $(this).attr("href");
+    changePosition(id);
+    event.preventDefault();
+  });
 
-    // On Click - Nav Links, href change position
-    $('.hero-item').click(function (event) {
-        var id = $(this).attr('href');
-        changePosition(id)
-        event.preventDefault();
-    })
+  // Animate Change Position
+  function changePosition(id) {
+    var target = $(id).offset().top;
 
-    // Animate Change Position
-    function changePosition(id) {
-
-        var target = $(id).offset().top;
-
-        if ($(window).width() > 1200) { //large screen
-            target = target - 50;
-        } else { // small screen 
-            target = target - 25;
-        }
-
-        if (id == "#section-ships") {
-            target = target - 15;
-        }
-
-        $('html, body').animate({ scrollTop: target }, 500);
-        window.location.hash = id;
-
+    if ($(window).width() > 1200) {
+      //large screen
+      target = target - 50;
+    } else {
+      // small screen
+      target = target - 25;
     }
 
+    if (id == "#section-ships") {
+      target = target - 15;
+    }
 
-    // hero video card
-    let heroVideoCard = document.querySelector(".video-card__video")
+    $("html, body").animate({ scrollTop: target }, 500);
+    window.location.hash = id;
+  }
+
+  // hero video card
+  let heroVideoCard = document.querySelector(".video-card__video");
+  if (heroVideoCard) {
     heroVideoCard.addEventListener("mouseover", function (e) {
-        heroVideoCard.play();
-    })
+      heroVideoCard.play();
+    });
     heroVideoCard.addEventListener("mouseout", function (e) {
-        heroVideoCard.pause();
-    })
+      heroVideoCard.pause();
+    });
+  }
 
+  // video modal
+  // TODO: to fix the issue of the first-load iframe, initialize the player every time the videoPlayButton and heroVideoCard are clicked
+  var iframe = document.getElementById("modal-video-iframe");
+  if (iframe) {
+    const vimeoPlayer = new Vimeo.Player(iframe);
+    const videoModal = document.querySelector("#videoModal");
+    const videoPlayButton = document.querySelector(".video-play-button");
 
-    // video modal
-    // TODO: to fix the issue of the first-load iframe, initialize the player every time the videoPlayButton and heroVideoCard are clicked
-    var iframe = document.getElementById('modal-video-iframe');
-    if(iframe){
-        const vimeoPlayer = new Vimeo.Player(iframe);  
-        const videoModal = document.querySelector("#videoModal");
-        const videoPlayButton = document.querySelector(".video-play-button");
-    
-        if (vimeoPlayer) {
-            // -- open / play
-            videoPlayButton.addEventListener('click', () => {
-                videoModal.style.display = 'flex';
-                body.classList.add('no-scroll');
-    
-                vimeoPlayer.play();
-            });
-    
-            heroVideoCard.addEventListener('click', () => {
-                videoModal.style.display = 'flex';
-                body.classList.add('no-scroll');
-                vimeoPlayer.play();
-            });
-    
-            // -- stop playing 
-            const stopVideoSections = [...document.querySelectorAll('.stop-video')];
-            stopVideoSections.forEach(section => {
-                section.addEventListener('click', () => {
-                    vimeoPlayer.pause();
-                });
-            })
-        }
-    
+    if (vimeoPlayer) {
+      // -- open / play
+      videoPlayButton.addEventListener("click", () => {
+        videoModal.style.display = "flex";
+        body.classList.add("no-scroll");
+
+        vimeoPlayer.play();
+      });
+
+      heroVideoCard.addEventListener("click", () => {
+        videoModal.style.display = "flex";
+        body.classList.add("no-scroll");
+        vimeoPlayer.play();
+      });
+
+      // -- stop playing
+      const stopVideoSections = [...document.querySelectorAll(".stop-video")];
+      stopVideoSections.forEach((section) => {
+        section.addEventListener("click", () => {
+          vimeoPlayer.pause();
+        });
+      });
     }
-    
+  }
 
+  // view all ships
+  const allShipsButton = document.querySelector("#all-ships-button");
+  const expandedShips = document.querySelector("#expanded-ships");
 
-    // view all ships
-    const allShipsButton = document.querySelector("#all-ships-button");
-    const expandedShips = document.querySelector("#expanded-ships");
+  // -- open / play
+  allShipsButton.addEventListener("click", () => {
+    if (expandedShips.classList.contains("expand")) {
+      expandedShips.classList.remove("expand");
+      allShipsButton.innerHTML = "View All Ships";
+    } else {
+      expandedShips.classList.add("expand");
+      allShipsButton.innerHTML = "Show Less";
+    }
+  });
 
+  // SLIDERS -------------------------------------------------------
+  // each ship's image area
+  new Swiper(".ship-card-image-area", {
+    slidesPerView: 1,
+    watchSlidesProgress: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+      dynamicMainBullets: 3,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+      enabled: true,
+    },
+  });
 
-    // -- open / play
-    allShipsButton.addEventListener('click', () => {
-        if (expandedShips.classList.contains('expand')) {
-            expandedShips.classList.remove('expand')
-            allShipsButton.innerHTML = "View All Ships"
-        } else {
-            expandedShips.classList.add('expand')
-            allShipsButton.innerHTML = "Show Less"
+  // itineraries slider
+  new Swiper("#itineraries-best-slider", {
+    spaceBetween: 15,
+    slidesPerView: 1.2,
+    watchSlidesProgress: true,
+    navigation: {
+      nextEl: ".itineraries-best-slider-btn-next",
+      prevEl: ".itineraries-best-slider-btn-prev",
+    },
+    breakpoints: {
+      600: {
+        slidesPerView: 2,
+      },
+      1000: {
+        slidesPerView: 3,
+      },
+      1300: {
+        slidesPerView: 4,
+      },
+    },
+  });
 
-        }
+  // themes slider
+  new Swiper("#styles-slider", {
+    spaceBetween: 15,
+    slidesPerView: 1.2,
+    watchSlidesProgress: true,
+    navigation: {
+      nextEl: ".styles-slider-btn-next",
+      prevEl: ".styles-slider-btn-prev",
+    },
+    breakpoints: {
+      600: {
+        slidesPerView: 2,
+      },
+      800: {
+        slidesPerView: 3,
+      },
+      1000: {
+        slidesPerView: 4,
+      },
+      1300: {
+        slidesPerView: 5,
+      },
+    },
+  });
 
-    });
-
-
-
-    // SLIDERS -------------------------------------------------------
-    // each ship's image area
-    new Swiper('.ship-card-image-area', {
-        slidesPerView: 1,
-        watchSlidesProgress: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-            dynamicBullets: true,
-            dynamicMainBullets: 3,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-            enabled: true
-        },
-
-    });
-
-    // itineraries slider
-    new Swiper('#itineraries-best-slider', {
-        spaceBetween: 15,
-        slidesPerView: 1.2,
-        watchSlidesProgress: true,
-        navigation: {
-            nextEl: '.itineraries-best-slider-btn-next',
-            prevEl: '.itineraries-best-slider-btn-prev',
-        },
-        breakpoints: {
-            600: {
-                slidesPerView: 2,
-            },
-            1000: {
-                slidesPerView: 3,
-            },
-            1300: {
-                slidesPerView: 4,
-            }
-        }
-    });
-
-    // themes slider
-    new Swiper('#styles-slider', {
-        spaceBetween: 15,
-        slidesPerView: 1.2,
-        watchSlidesProgress: true,
-        navigation: {
-            nextEl: '.styles-slider-btn-next',
-            prevEl: '.styles-slider-btn-prev',
-        },
-        breakpoints: {
-            600: {
-                slidesPerView: 2,
-            },
-            800: {
-                slidesPerView: 3,
-            },
-            1000: {
-                slidesPerView: 4,
-            },
-            1300: {
-                slidesPerView: 5,
-            }
-        }
-    });
-
-    // experiences slider
-    new Swiper('#experiences-slider', {
-        spaceBetween: 30,
-        slidesPerView: 1,
-        watchSlidesProgress: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-            dynamicBullets: true,
-            dynamicMainBullets: 3,
-        },
-        navigation: {
-            nextEl: '.experiences-slider-btn-next',
-            prevEl: '.experiences-slider-btn-prev',
-        },
-        breakpoints: {
-            600: {
-                slidesPerView: 2
-            },
-            800: {
-                slidesPerView: 3,
-            }
-        }
-    });
-
-
+  // experiences slider
+  new Swiper("#experiences-slider", {
+    spaceBetween: 30,
+    slidesPerView: 1,
+    watchSlidesProgress: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+      dynamicMainBullets: 3,
+    },
+    navigation: {
+      nextEl: ".experiences-slider-btn-next",
+      prevEl: ".experiences-slider-btn-prev",
+    },
+    breakpoints: {
+      600: {
+        slidesPerView: 2,
+      },
+      800: {
+        slidesPerView: 3,
+      },
+    },
+  });
 });
-
-
-
