2bb1f25b5a5357892c5df5b09c413c53dec2437e
Ryan <rlsystems@outlook.com>
2025-06-24 13:30:12 +0200

    removed reply script

--- includes/theme-config.php
+++ includes/theme-config.php
@@ -145,37 +145,6 @@ function wp_dereg_script_comment_reply()
 }
 add_action('init', 'wp_dereg_script_comment_reply');
 
-add_action('wp_head', 'wp_reload_script_comment_reply');
-function wp_reload_script_comment_reply()
-{
-?>
-    <script>
-        //Function checks if a given script is already loaded
-        function isScriptLoaded(src) {
-            return document.querySelector('script[src="' + src + '"]') ? true : false;
-        }
-        //When a reply link is clicked, check if reply-script is loaded. If not, load it and emulate the click
-        document.getElementsByClassName("comment-reply-link").onclick = function() {
-            if (!(isScriptLoaded("/wp-includes/js/comment-reply.min.js"))) {
-                var script = document.createElement('script');
-                script.src = "/wp-includes/js/comment-reply.min.js";
-                script.onload = emRepClick($(this).attr('data-commentid'));
-                document.head.appendChild(script);
-            }
-        }
-        //Function waits 50 ms before it emulates a click on the relevant reply link now that the reply script is loaded
-        function emRepClick(comId) {
-            sleep(50).then(() => {
-                document.querySelectorAll('[data-commentid="' + comId + '"]')[0].dispatchEvent(new Event('click'));
-            });
-        }
-        //Function does nothing, for a given amount of time
-        function sleep(time) {
-            return new Promise((resolve) => setTimeout(resolve, time));
-        }
-    </script>
-<?php
-}
 
 
 // Honeypot
